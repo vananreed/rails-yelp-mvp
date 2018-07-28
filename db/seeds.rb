f60d31c48576e1c3dcbd8a6ab610ec837f2541df
Reed Van Anrooy <reed.vananrooy@gmail.com>
2018-07-28 12:59:10 +0800

    Restaurant and Review Controllers created with correct nested routes. Views needed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  restaurant = Restaurant.new(
+    name: Faker::FunnyName.name,
+    category: "french",
+    address: Faker::Address.street_address,
+    phone_number: Faker::Number.number(10),
+  )
+  restaurant.save
+end
